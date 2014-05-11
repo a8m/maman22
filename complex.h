41801200a26b4fa1197288c11226b1929926d828
Ariel Mashraki <ariel@mashraki.co.il>
2014-05-11 23:12:16 +0300

    init commit

---
+++ complex.h
@@ -0,0 +1,30 @@
+/*
+ * =========================================================================================
+ * name        : mmn_22/main.c
+ * author      : Ariel Mashraki, ID 301......
+ * email       : ariel@.....
+ * description : description....
+ * =========================================================================================
+ */
+
+#ifdef COMPLEX_H_
+#define COMPLEX_H_
+
+/*read.. should i used them?*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+#define ....
+
+typedef struct compleX{
+    int a,b;
+} compleX;
+
+enum nameOfEnum{};
+
+/*functions declartion(void,int, more...)*/
+
+
+#endif /* COMPLEX_H_ */
